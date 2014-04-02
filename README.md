9dfa36aad2de4a03c1bb4569052e047b3f55450c
Jason Rikard <jsnrkd@gmail.com>
2014-04-02 08:10:19 -0400

    Added example

--- README.md
+++ README.md
@@ -1,15 +1,35 @@
-Fake-this is a simple utility wrapper of the the PHP [Faker](https://github.com/fzaninotto/Faker) project for anonymizing your production dataset to be distributed and used for testing.
+Fake-this is a boilerplate for the PHP [Faker](https://github.com/fzaninotto/Faker) library for anonymizing your production dataset to be distributed and used for testing.  It is discussed further at [Chef-gm](http://jsnrkd.github.io/chef-gm).
+
+Note: Backup your data before running!
+
+####Dependencies
+
+- git
+- php
+- composer
+- mysql client
 
 # How to use
+
+Setup
 
     $ git clone https://github.com/Jsnrkd/faker-this
     $ cd faker-this
     $ composer install
-    $ nano config.php
-    $ php fake.php
+
+Code your logic
+
+    $ nano fake.php
+
+Anonymize
+
+    $ php fake.php <host> <mysql_user> <password> <dbPrefix>
+
+Distribute 
+
     $ mysqldump -u [user] -p[password] --all-databases | gzip > fake_data.sql.gz
 
-Distribute file then import
+Import
 
     $ gunzip < fake_data.sql.gz | mysql -u [user] -p[password]
 
--- fake.php
+++ fake.php
@@ -1,32 +1,54 @@
 <?php
+/*
+
+CLI args order= host user password dbPrefix
+
+Example using faker https://github.com/fzaninotto/Faker
+
+This takes arguments from the command line so that the utility could be used in automation. 
+
+The example changes the name and address of a user in a MySQL table.
+
+Uses MySQLi but can be easily modified to PDO.
+*/
+
+
+$host = $argv[1];
+$user = $argv[2];
+$password = $argv[3];
+$dbPrefix = $argv[4];
 
 // require the Faker autoloader
 require_once 'vendor/autoload.php';
+$faker = Faker\Factory::create();
+$db = new mysqli($host, $user, $password);
 
-// // setup the autoloading
-// require_once '/vendor/autoload.php';
-use Propel\Runtime\Propel;
-use Propel\Runtime\Connection\ConnectionManagerSingle;
-$serviceContainer = Propel::getServiceContainer();
-$serviceContainer->setAdapterClass('bookstore', 'mysql');
-$manager = new ConnectionManagerSingle();
-$manager->setConfiguration(array (
-  'dsn'      => 'mysql:host=localhost;dbname=change',
-  'user'     => '',
-  'password' => '',
-));
-$serviceContainer->setConnectionManager('bookstore', $manager);
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
 
-// use Monolog\Logger;
-// use Monolog\Handler\StreamHandler;
-// $defaultLogger = new Logger('defaultLogger');
-// $defaultLogger->pushHandler(new StreamHandler('/var/log/propel.log', Logger::WARNING));
-// $serviceContainer->setLogger('defaultLogger', $defaultLogger);
+#get database that start with prefix and end with a number
+$db_result = $db->query('SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME REGEXP "^'.$dbPrefix.'.*[0-9]$"');
 
-// $generator = \Faker\Factory::create();
-// $populator = new Faker\ORM\Propel\Populator($generator);
-// $populator->addEntity('Author', 5);
-// $populator->addEntity('Book', 10);
-// $insertedPKs = $populator->execute();
+while($row = $db_result->fetch_array(MYSQLI_NUM)){
+	$db_name = $row[0];
+	echo 'Anonymizing '.$db_name."\n";
+	#Adults
+	if(!$result = $db->query("SELECT AdultID FROM $db_name.Adult")){
+	    die('There was an error running the query [' . $db->error . ']');
+	}
 
-
+	while($row = $result->fetch_assoc()){
+	    #echo $row['AdultID'];
+	    $fName = $faker->firstName;
+	    $lName = $faker->lastName;
+	    $streetAddress = $faker->streetAddress;
+	    $stmt = $db->prepare(
+	    	"UPDATE $db_name.Adult 
+	    	SET `first_name` = ?, `last_name` = ? , `Address1` = ?, `email` = '', `latitude` = '', 
+	    	`longitude` = ''
+	    	WHERE `AdultID` = ?");
+	    $stmt->bind_param('sssi', $fName, $lName, $streetAddress, $row['AdultID']);
+	    $stmt->execute();
+	}
+}
